335315137d7357e6d0e586680ace15b3d51bf262
Emeric Kasbarian <emerick42@gmail.com>
2015-05-04 07:32:36 +0200

    Add the Guzzle bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Misd\GuzzleBundle\MisdGuzzleBundle(),
             new App\Bundle\MainBundle\AppMainBundle(),
         );
 
--- src/App/Bundle/MainBundle/Services/Autocompleter/Handler/SpeedruncomHandler.php
+++ src/App/Bundle/MainBundle/Services/Autocompleter/Handler/SpeedruncomHandler.php
@@ -4,12 +4,30 @@ namespace App\Bundle\MainBundle\Services\Autocompleter\Handler;
 
 use App\Bundle\MainBundle\Form\Model\Submission;
 use App\Bundle\MainBundle\Services\Autocompleter\Exception\InvalidUrlException;
+use Guzzle\Http\ClientInterface;
 
 /**
  * Fetch data from a Speedrun.com run page
  */
 class SpeedruncomHandler implements HandlerInterface
 {
+    /**
+     * The web client used to fetch the URL content
+     *
+     * @var ClientInterface
+     */
+    private $client;
+
+    /**
+     * __construct
+     *
+     * @param ClientInterface $client
+     */
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
      * {@inheritdoc}
      */

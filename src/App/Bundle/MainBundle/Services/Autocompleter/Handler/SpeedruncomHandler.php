94f1ff6eccd5ac453006e7af3fa57ae51a0fb215
Emeric Kasbarian <emeric.kasbarian@elao.com>
2015-06-24 21:20:12 +0200

    Fix a bug with the Speedruncom mis-autocompleting platform

--- src/App/Bundle/MainBundle/Services/Autocompleter/Handler/SpeedruncomHandler.php
+++ src/App/Bundle/MainBundle/Services/Autocompleter/Handler/SpeedruncomHandler.php
@@ -184,11 +184,11 @@ class SpeedruncomHandler implements HandlerInterface
      */
     private function parseNote($note)
     {
-        $pattern = '/Played on (?P<platform>[\w ]*) (?:\[\w+\])* (?:on (?P<date>\d{4}-\d{2}-\d{2}))?\./';
+        $pattern = '/Played on (?P<platform>[\w ]*)(?: (?:\[\w+\])*)? (?:on (?P<date>\d{4}-\d{2}-\d{2}))?\./';
         $matches = [];
         preg_match($pattern, $note, $matches);
 
-        $platform = $matches['platform'];
+        $platform = (isset($matches['platform']) ? $matches['platform'] : '');
         $date     = (isset($matches['date']) ? new \DateTime($matches['date']) : null);
 
         return [$platform, $date];
